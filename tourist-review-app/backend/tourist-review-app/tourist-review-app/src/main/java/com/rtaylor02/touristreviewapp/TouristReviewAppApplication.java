57220021076b52dfd09bc20afef479a0bf894334
rtaylor02 <taylor.rod.j@gmail.com>
2022-12-08 12:21:04 +0000

    First commit for tourist-review-app
    Basic backend and frontend applications added: Spring Boot and React

---
+++ tourist-review-app/backend/tourist-review-app/tourist-review-app/src/main/java/com/rtaylor02/touristreviewapp/TouristReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.touristreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristReviewAppApplication.class, args);
+	}
+
+}
---
+++ tourist-review-app/backend/tourist-review-app/tourist-review-app/src/test/java/com/rtaylor02/touristreviewapp/TouristReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.touristreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
